1ee019ec0c8ab7dfd67c4374dab5a7d4516dfa49
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-05-02 15:28:32 -0700

    Step 1 - Create a new file inside the store folder.

---
+++ src/app/store/app.selectors.ts
@@ -0,0 +1,7 @@
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { AppState } from './app.reducer';
+export const selectApp = createFeatureSelector<AppState>('app');
+export const selectUsers = createSelector(
+  selectApp,
+  (state: AppState) => state.users
+);
