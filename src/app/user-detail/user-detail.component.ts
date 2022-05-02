03ecc09265ade0d5002f32375ca0e94a3bad5f08
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-05-02 15:37:42 -0700

    Step 3 - 2 observables so we can subscribe to respective store

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IUser } from '../core/interfaces/user.interface';
+//step 3
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,10 +10,16 @@ import { IUser } from '../core/interfaces/user.interface';
   styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent implements OnInit, OnDestroy {
-  user: IUser = null;
-  similarUsers: IUser[] = [];
+  // user: IUser = null;
+  // similarUsers: IUser[] = [];
+  // isComponentAlive: boolean;
+
+  //step 3
+  user$: Observable<IUser> = null;
+  similarUsers$: Observable<IUser[]> = null;
   isComponentAlive: boolean;
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
     this.isComponentAlive = true;
